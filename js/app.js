c5f78dc7afb624a05d99b8eda54f183a47c94c16
Ridwan <ridwanishola97@gmail.com>
2020-04-24 17:08:14 +0100

    simple template

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+var text = document.getElementById("txt");
+var detail = document.getElementById("emptyText");
+
+var client_id = " Iv1.28dec2a34b9057bb";
+var client_secret = "12f8f940a393cf7789b56a763b145c74f5ddb760";
